ed6636a3e8c439e39b39f0d570dfd354fbcac405
jasmo2 <jasmo2.com@gmail.com>
2018-03-19 13:14:05 -0500

    initial commit

---
+++ presentation.ts
@@ -0,0 +1,27 @@
+//// Typing
+
+/// Basic Types
+
+// var isDone: boolean = false;
+// var decimal: number = 6;
+// var hex: number = 0xf00d;
+// var binary: number = 0b1010;
+// var octal: number = 0o744;
+
+// var list: number[] = [1, 2, 3];
+// var listB: (number|string)[] = [1, '2', 3]; // <-- just acept the types of variables that is declare.
+
+// var anyVariable: any = 'could be a string';
+// anyVariable = true;
+// anyVariable = 3;
+// ¯\_(ツ)_/¯  // <-- Is the less restrictive case in Typescript
+
+
+/// Preventing overwrite
+// var a = 123;
+// a = '123'; // <-- posible overwrite
+
+// let boolean = true;
+// boolean = 'true';
+
+// let booleanB: boolean = 'false';
